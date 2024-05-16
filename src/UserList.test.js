46271a36db937f5d68068709d115d9b994a61508
Oscar Lagatta <oscar.lagatta@whistl.co.uk>
2024-05-16 17:51:00 +0100

    Update App.js with user management functionality and upgrade dependencies
    Replaced the generic content of the App.js file with user management functionality and created related component imports. Also, updated the dependencies in package.json and refreshed the dependency tree in package-lock.json. Created 'UserList.test.js' file for testing UserList component.

---
+++ src/UserList.test.js
@@ -0,0 +1,45 @@
+import {getByText, render, screen, within} from "@testing-library/react";
+import UserList from "./UserList";
+import userList from "./UserList";
+
+test('renders one row per user', () => {
+    const users = [
+        {name: 'jane', email: 'jane@jane.com'},
+        {name: 'sam', email: 'sam@same.com'},
+    ];
+    // render the component
+    // const { container } =
+    render(<UserList users={users}/>);
+
+    // screen.logTestingPlaygroundURL();
+    // find all the rows in the table
+
+    // const rows = screen.getAllByRole('row');
+
+    const rows = within(screen.getByTestId('users')).getAllByRole('row');
+
+    // const table = container.querySelector('table');
+
+    // eslint-disable-next-line
+    // const rows = container.querySelectorAll('tbody tr');
+
+    // Assertion: correct number of rows in the table
+    expect(rows).toHaveLength(2);
+
+});
+
+test('render the email and name of each user', () => {
+    const users = [
+        {name: 'jane', email: 'jane@jane.com'},
+        {name: 'sam', email: 'sam@same.com'},
+    ];
+
+    render(<UserList users={users}/>);
+
+    for(let user of users) {
+        const name = screen.getByRole('cell', { name: user.name });
+        const email = screen.getByRole('cell', { name: user.email });
+        expect(name).toBeInTheDocument();
+        expect(email).toBeInTheDocument();
+    }
+});
